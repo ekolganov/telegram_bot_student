79a5c09b83da0fe982fb57a7da3772541db13cc9
karamel32 <29039986+karamel32@users.noreply.github.com>
2021-04-11 01:56:45 +0300

    1st commit

--- createdb.sql
+++ createdb.sql
@@ -5,11 +5,6 @@ create table student(
     description text
 );
 
-insert into student(id, full_name, grade_number, description) values
-(1, "Петров Евгений", "6 класс", "не очень прилежный"),
-(2, "Пупкин Василий", "6 класс", "прилежный"),
-(3, "Герасимов Гена", "7 класс", "отличнег");
-
 create table themes(
     id integer primary key,
     themes_grade_number varchar(255),
@@ -18,27 +13,31 @@ create table themes(
     FOREIGN KEY(themes_grade_number) REFERENCES student(grade_number)
 );
 
-insert into themes(id, themes_grade_number, theme_name) values
-(1,"6 класс","Глагол. Контрольная работа с грамматическим заданием"),
-(2,"6 класс","Глагол. Правописание суффиксов глаголов -ОВА- (-ЕВА-), -ЫВА- (-ИВА-)"),
-(3,"7 класс","Наречие. Суффиксы О и А на конце наречий"),
-(4,"7 класс","Причастие. Гласные в суффиксах причастий");
-
+create table code_status(
+    id integer primary key,
+    status_name varchar(255)
+);
 
 create table marks(
     theme text primary key,
     student_id integer,
-    theme_status integer
+    theme_status integer,
 
-    FOREIGN KEY(theme) REFERENCES themes(theme_name)
-    FOREIGN KEY(student_id) REFERENCES student(id)
+    FOREIGN KEY(theme) REFERENCES themes(theme_name),
+    FOREIGN KEY(student_id) REFERENCES student(id),
     FOREIGN KEY(theme_status) REFERENCES code_status(id)
-)
+);
 
-create table code_status(
-    id integer primary key,
-    status_name varchar(255)
-)
+insert into student(id, full_name, grade_number, description) values
+    (1, "Петров Евгений", "6 класс", "не очень прилежный"),
+    (2, "Пупкин Василий", "6 класс", "прилежный"),
+    (3, "Герасимов Гена", "7 класс", "отличнег");
+
+insert into themes(id, themes_grade_number, theme_name) values
+    (1,"6 класс","Глагол. Контрольная работа с грамматическим заданием"),
+    (2,"6 класс","Глагол. Правописание суффиксов глаголов -ОВА- (-ЕВА-), -ЫВА- (-ИВА-)"),
+    (3,"7 класс","Наречие. Суффиксы О и А на конце наречий"),
+    (4,"7 класс","Причастие. Гласные в суффиксах причастий");
 
 insert into code_status(id, status_name) values
     (1, "undefined"),
